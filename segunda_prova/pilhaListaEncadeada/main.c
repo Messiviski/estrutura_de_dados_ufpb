4d8767749858da8edde3b30677f850b5644c865e
Messias <messiasolimpio@hotmail.com>
2021-10-20 20:17:22 -0300

    update gitignore

--- segunda_prova/pilhaListaEncadeada/main.c
+++ segunda_prova/pilhaListaEncadeada/main.c
@@ -7,15 +7,27 @@ int main(void) {
 
   pilha = pilha_cria();
 
-  pilha_push(pilha, "r");
-  pilha_push(pilha, "e");
-  pilha_push(pilha, "v");
-  pilha_push(pilha, "i");
-  pilha_push(pilha, "v");
-  pilha_push(pilha, "e");
-  pilha_push(pilha, "r");
+  pilha_push(pilha, 'r');
+  pilha_push(pilha, 'e');
+  pilha_push(pilha, 'v');
+  pilha_push(pilha, 'i');
+  pilha_push(pilha, 'v');
+  pilha_push(pilha, 'e');
+  pilha_push(pilha, 'r');
 
-  printf(" %c ", pilha_pop(pilha));
+  printf("\n%c", pilha_pop(pilha));
+  printf("%c", pilha_pop(pilha));
+  printf("%c", pilha_pop(pilha));
+  printf("%c", pilha_pop(pilha));
+  printf("%c", pilha_pop(pilha));
+  printf("%c", pilha_pop(pilha));
+  printf("%c\n", pilha_pop(pilha));
+
+  if(pilha_vazia(pilha)) {
+    printf("A pilha esta vazia!\n");
+  } else {
+    printf("A pilha nao esta vazia!\n");
+  }
 
   pilha_libera(&pilha);
 
